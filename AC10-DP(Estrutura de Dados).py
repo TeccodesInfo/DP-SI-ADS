ef8e05f54199d1ede06751cd0f1f8379bdfa9c09
Codes_TEC <36265813+TeccodesInfo@users.noreply.github.com>
2019-05-16 11:13:06 -0300

    AC_10(DP-Estrutura de Dados)
    Adicionado Código da AC!!!

---
+++ AC10-DP(Estrutura de Dados).py
@@ -0,0 +1,15 @@
+Clientes = ["Deise","Pedro","Kássia"]
+Mesa = ["1, 7, 4"]
+Pedidos = ["Lanches","Bebidas","Sobremesas"]
+o = Clientes + Mesa + Pedidos
+print(o) 
+
+##############################################
+Clientes = ["Deise","Pedro","Kássia"]
+print(Clientes[::-1])
+##############################################
+Pedidos = ["Lanches","Bebidas","Sobremesas"]
+print(Pedidos[::-1])
+
+
+
